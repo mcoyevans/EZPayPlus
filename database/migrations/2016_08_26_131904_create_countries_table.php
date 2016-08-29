d625f34edb3a77b6309b780d4a37ede2321e3257
Marco Paco <marcopaco1230@gmail.com>
2016-08-29 22:34:46 +0800

    Added countries sql file
    Added countries sql file located at '/public/assets/countries.sql'  .
    Manually import this file to load the list of countries with the
    corresponding code.

--- database/migrations/2016_08_26_131904_create_countries_table.php
+++ database/migrations/2016_08_26_131904_create_countries_table.php
@@ -14,6 +14,7 @@ class CreateCountriesTable extends Migration
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->increments('id');
+            $table->char('code', 2);
             $table->string('name');
             $table->timestamps();
         });
